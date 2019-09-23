9a196537f9f964c85a23dca122fb0e584fb1ede2
sfiler2112 <42945574+sfiler2112@users.noreply.github.com>
2019-09-23 07:47:38 -0600

    Add files via upload

--- CaveQuest.java
+++ CaveQuest.java
@@ -1,22 +1,39 @@
 /*
- *  Name:
- *  Date: 4/14/2019
+ *  Name: CaveQuest
+ *  Date: 9/22/2019
  *  Author: Sean Filer
- *  Purpose: 
+ *  Purpose: Another toy program to play with multithreading.  This class will provide a gui to display the progress of the quest.
  */
 package cavequest;
 
-/**
- *
- * @author Sean
- */
-public class CaveQuest {
+import javax.swing.*;
+import java.awt.event.*;
 
-    /**
-     * @param args the command line arguments
-     */
+public class CaveQuest extends JFrame implements ActionListener{
+
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        CaveQuest mainQuest = new CaveQuest();
     }
     
+    public CaveQuest(){
+        super("CaveQuest");
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        displayStartingScreen();
+    }
+    
+    public void displayStartingScreen(){ 
+        this.getContentPane().removeAll();
+        
+        JPanel testPanel = new JPanel();
+        testPanel.add(new JLabel("hey ;)"));
+        this.getContentPane().add(testPanel);
+        this.pack();
+    }
+    
+    public void actionPerformed(ActionEvent e){
+        /*What to do when an action is performed*/
+    }
 }
